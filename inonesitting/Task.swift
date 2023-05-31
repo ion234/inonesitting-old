dc04cf5e30374f1ac204457819b153a45d3fdced
Allen Lin <al319966@gmail.com>
2023-05-30 20:31:14 -0400

    Initial Commit

---
+++ inonesitting/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  inonesitting
+//
+//  Created by Allen Lin on 5/18/23.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title = ""
+    @Persisted var completed = false
+    
+}
