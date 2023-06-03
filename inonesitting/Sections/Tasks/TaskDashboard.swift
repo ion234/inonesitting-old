ae883a7eecbd4974a616ad9c67bea5750025b26b
Abe Park <abepark013@gmail.com>
2023-06-03 19:46:22 -0400

    UI Commit

--- inonesitting/Sections/Tasks/TaskDashboard.swift
+++ inonesitting/Sections/Tasks/TaskDashboard.swift
@@ -19,26 +19,26 @@ struct TaskDashboard: View {
             //MARK: Top Bar
             HStack {
                 VStack(spacing: 8) {
-                    Text("Your Tasks")
-                        //.font(.custom("", fixedSize: 32))
-                        .font(.largeTitle)
+                    Text("Dashboard")
+                        .font(.title)
                         .fontWeight(.heavy)
+                        .fontDesign(.serif)
                         .frame(maxWidth: .infinity, alignment: .leading)
                     
                     Text("Today, \(Date().formatted(.dateTime.month().day()))")
-                        //.font(.custom("", fixedSize: 16.5))
                         .font(.headline)
-                        .fontWeight(.regular)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
                         .frame(maxWidth: .infinity, alignment: .leading)
                     
                 }
-                .padding(.leading, 36)
+                .padding(.leading, 32)
                 
                 Button {
                     showAddView = true
                 } label: {
                     Image(systemName: "plus")
-                        .font(.custom("", fixedSize: 25))
+                        .font(.system(size: 22))
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .padding(8)
@@ -48,70 +48,15 @@ struct TaskDashboard: View {
                 .buttonStyle(.plain)
                     
             }
-            .padding(.top, 32)
+            
+            .padding(.top, 24)
             .padding(.bottom)
             
             Divider()
-                .padding(.horizontal, 36)
-            
+               
             ScrollView(showsIndicators: false) {
                 
-                VStack(spacing: 4) {
-                    Text("Today's Progress")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading, .vertical])
-                    
-                    Text("10/12 Tasks")
-                        .font(.caption)
-                        .fontWeight(.medium)
-                        .foregroundColor(.gray)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    
-                    Text("\(83) %")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .fontWeight(.heavy)
-                        .padding(.horizontal)
-                        
-                    ProgressView(value: 10, total: 12)
-                        .tint(.white)
-                        .padding([.horizontal, .bottom])
-                    
-                }
-                .background(Color("boldElements"))
-                .cornerRadius(12)
-                .padding(.horizontal, 32)
-                .padding(.vertical)
-                
-                Text("All Day")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal, 32)
-                
-                HStack {
-                    Text("Mow the lawn (example)")
-                        .font(.body)
-                        .fontWeight(.medium)
-                        .padding(.leading, 32)
-                    
-                    Spacer()
-                    
-                    
-                    Image(systemName: "checkmark")
-                        .padding(.trailing, 32)
-                }
-                .padding(.top, 12)
-                
-                Divider()
-                    .padding(.horizontal, 32)
             }
-                    
         }
         .background(Color.background)
         .sheet(isPresented: $showAddView) {
